183b0db339f17ab23d243fe0017c01c00ade1bf1
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-12-11 20:44:02 +0200

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import React from "react";
+import BasicCard from "./components/Card";
+import Navbar from "./components/Navbar";
+import "./styles.css";
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Search from "./components/Search";
+
+
+export default function App() {
   return (
-    <div>
-      Hello world!
+    <div className="App">
+      <Navbar />
+      <Search/>
+      <BasicCard/>
+
     </div>
   );
 }
-
-export default App;
---
+++ client/src/components/MenuItems.js
@@ -0,0 +1,29 @@
+const menuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "My tickets",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "Recommended tickets",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "Contact",
+    url: "#",
+    cName: "nav-links"
+  },
+  {
+    title: "Log in",
+    url: "#",
+    cName: "nav-links"
+  }
+];
+
+export default menuItems;
