916e3c256d0d5f78fd71fd483211073f0540fc67
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-12-11 20:53:28 +0200

    finished resservation

--- client/node_modules/react-search-field/tests/test.setup.js
+++
@@ -1,9 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-
-function testConfig() {
-  configure({ adapter: new Adapter() });
-}
-
-module.exports = testConfig();
