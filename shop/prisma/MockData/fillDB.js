31fc8b7a884d05fe4dc2be15a152a1613e916324
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-12-14 21:12:14 +0200

    before kafka

--- shop/prisma/MockData/fillDB.js
+++ shop/prisma/MockData/fillDB.js
@@ -3,15 +3,27 @@ const prisma = new PrismaClient()
 import user from './customer.js'
 import match from './matches.js'
 import tickets from './tickets.js'
+import fs from 'fs'
 
 
 async function main(){
     
     
-    const d = await prisma.ticket.createMany({
-        data:[...tickets],
+    // const d = await prisma.ticket.createMany({
+    //     data:[...tickets],
+    // })
+
+
+    // i want to get the number of tickets for the first match where the category is 1
+    const m = await prisma.ticket.findMany({
+        where:{
+            matchId:2,
+            isHold:true,
+        },
     })
 
+
+    console.log(m)
 
 
 }
