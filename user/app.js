704f84137d9181f4aaba0f3d8183cd319235c496
AlHassanHK <al.hassan.elkady@gmail.com>
2022-12-16 21:12:21 +0200

    fix app.js

--- user/app.js
+++ user/app.js
@@ -1,26 +1,20 @@
-const express = require('express');
-const createError = require('http-errors');
-const morgan = require('morgan');
-const cors = require('cors');
-const cookieParser =require('cookie-parser') 
-require('dotenv').config();
-
+import express from "express";
+import httpErrors from "http-errors";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/userRoutes";
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser())
-app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(morgan("dev"));
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
-});
-
-app.use('/api', require('./routes/api.route'));
-
-
+app.use("/api/users", userRouter);
 
 app.use((req, res, next) => {
   next(createError.NotFound());
@@ -30,9 +24,9 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send({
     status: err.status || 500,
-    message: err.message,
+    message: err.message
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
