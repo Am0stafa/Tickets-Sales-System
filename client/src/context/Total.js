3b738620dd954463dbe3ab9b53e84fc4b1d93494
marawan1805 <marawan1805@gmail.com>
2022-12-21 10:19:08 +0200

    merge

--- client/src/context/Total.js
+++ client/src/context/Total.js
@@ -1,61 +1,40 @@
-import { useState } from 'react'
-import { createContext } from 'react'
+import { useState } from "react";
+import { createContext } from "react";
 
+const AppContext = createContext();
 
-const AppContext = createContext()
+export const AppContextProvider = ({ children }) => {
+  const [total, setTotal] = useState(0);
+  const [choices, setChoices] = useState({
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+  });
+  const priceForCategory = {
+    1: 200,
+    2: 400,
+    3: 800,
+    4: 1200,
+  };
 
-export const AppContextProvider = ({children}) => {
+  const calculateTotal = ({ amount, category }) => {
+    const newChoices = { ...choices };
+    newChoices[category] = amount * priceForCategory[category];
+    setChoices(newChoices);
+    //! loop through the newChoices object and add up the values
+    const newTotal = Object.values(newChoices).reduce(
+      (acc, curr) => acc + curr,
+      0
+    );
+    setTotal(newTotal);
+  };
 
-    const [total, setTotal] = useState(0)
-
-    const calculateTotal = ({amount,category}) => {
-    
-        let t = 0
-        if(parseInt(category) === 1){
-            if(amount === 0){
-                if(total > 0){
-                    t = total - 200
-                }
-            }else{
-                t = amount * 200
-            }
-        }
-        else if(parseInt(category) === 2){
-            if(amount === 0){
-                if(total > 0){
-                    t = total - 400
-                }
-            }else{
-                t = amount * 400
-            }
-        }
-        else if(parseInt(category) === 3){
-            if(amount === 0){
-                if(total > 0){
-                    t = total - 800
-                }
-            }else{
-                t = amount * 800
-            }
-        }
-        else if(parseInt(category) === 4){
-            if(amount === 0){
-                if(total > 0){
-                    t = total - 1200
-                }
-            }else{
-                t = amount * 1200
-            }
-        }
-        setTotal((prev) => t+prev)
-    }
-
-    
   return (
-    <AppContext.Provider value={{total,calculateTotal}}>
-        {children}
+    <AppContext.Provider value={{ total, calculateTotal }}>
+      {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 
-export default AppContext
+export default AppContext;
